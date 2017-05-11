de3dfd5330a6a9acdb7cd77f770aa6be3023e6bd
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-05-11 20:38:57 +0000

    added order details

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ActiveRecord::Base
 
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
-
   belongs_to :category
 
   validates :name, presence: true
