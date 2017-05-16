c84cc7c2133db5db46bbe6287954edbf9bb9f1e0
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-05-16 21:25:37 +0000

    see product details

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true  do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They see product details page" do
+    visit root_path
+    first('article.product').find_link('Add').click
+    save_screenshot 'test3.png'
+    expect(page).to have_content('My Cart (1)')
+  end
+
+
+end
